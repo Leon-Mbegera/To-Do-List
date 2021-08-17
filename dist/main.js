d3206de9428572f722c16f7ab320271173e6ac4c
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-08-17 13:25:19 +0300

    Write a simple alert statement and display on browser to make sure everything is working correctly

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('here i come');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHFCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG8tZG8tbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJhbGVydCgnaGVyZSBpIGNvbWUnKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
