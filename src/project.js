d444a646af9356e0e49673c73262ea5995da3b7a
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-08-26 23:37:15 +0300

    Put project and class constructors into separate modules

---
+++ src/project.js
@@ -0,0 +1,20 @@
+export class Project {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  addTodo = (todo) => {
+    this.todos.push(todo);
+  }
+
+  getTodo = (todoTitle) => {
+    let myTodo;
+    this.todos.forEach((todo) => {
+      if (todo.Title() === todoTitle) {
+        myTodo = todo;
+      }
+    });
+    return myTodo;
+  }
+}
---
+++ src/todo.js
@@ -0,0 +1,8 @@
+export class Todo {
+  constructor(title, description, priority, dueDate) {
+    this.title = title;
+    this.description = description;
+    this.priority = priority;
+    this.dueDate = dueDate;
+  }
+}
